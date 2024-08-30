8686fb12756825d1690438cd1a2704dccb3f1cf5
priyatomar2305 <tomarofficials@gmail.com>
2024-08-30 11:42:12 +0530

    some res

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,136 +1,111 @@
-import Insta from '../img/SH.png';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
-import { useContext, useEffect } from 'react';
+import Insta from "../img/SH.png";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
 import "../css/Navbar.css";
-import { useState } from 'react';
-import { LoginContext } from '../context/LoginContext';
-LoginContext
+import { useState } from "react";
+import { LoginContext } from "../context/LoginContext";
+LoginContext;
 
+export default function Navbar({ login, setshowLogout }) {
+  const navigate = useNavigate();
 
-export default function Navbar({login,setshowLogout}) {
-     const navigate= useNavigate();
+  const loginStatus = () => {
+    const token = localStorage.getItem("jwt");
+    const user = JSON.parse(localStorage.getItem("user"));
 
-  
-  
- const loginStatus = () => {
- const token = localStorage.getItem("jwt")
-  const user = JSON.parse(localStorage.getItem("user"));
+    if (login || token) {
+      return (
+        <>
+          <Link to={`/myprofile`}>
+            <li>Profile</li>
+          </Link>
 
-
-
-   if (login || token) {
-    return (
-      <>
-
-        <Link to={`/myprofile`}>
-          <li>Profile</li>
-        </Link>
-
-        <Link to={"/myfollowing"}>
-          <li>following</li>
-        </Link>
-        <Link to="/createpost">
-          {" "}
-          <li>createpost</li>
-        </Link>
-        <button className='logout'
-          onClick={() => {
-setshowLogout(true)
-          }}
-        >
-          logout
-        </button>
-      </>
-    );
-   } else {
-     
-     
-     return <>
-   <Link to="/signup">
+          <Link to={"/myfollowing"}>
+            <li>following</li>
+          </Link>
+          <Link to="/createpost">
+            {" "}
+            <li>createpost</li>
+          </Link>
+          <button
+            className="logout"
+            onClick={() => {
+              setshowLogout(true);
+            }}
+          >
+            logout
+          </button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Link to="/signup">
             {" "}
             <li>SignUp</li>
           </Link>
           <Link to="/login">
             {" "}
             <li>Login</li>
-       </Link>
-       
-     
-   </>
-      
-}
- }
-  
- const loginMobile = () => {
-   const token = localStorage.getItem("jwt");
-   const user = JSON.parse(localStorage.getItem("user"));
+          </Link>
+        </>
+      );
+    }
+  };
 
-   if (login || token) {
-     return (
-       <>
-         <Link to={"/"}>
-           <span className="material-symbols-outlined">home</span>
-         </Link>
+  const loginMobile = () => {
+    const token = localStorage.getItem("jwt");
+    const user = JSON.parse(localStorage.getItem("user"));
 
-         <Link to={`/myprofile`}>
-           <span className="material-symbols-outlined">person</span>{" "}
-         </Link>
+    if (login || token) {
+      return (
+          <div className="nav-mobile">
+            <Link to={"/"}>
+              <span className="material-symbols-outlined">home</span>
+            </Link>
 
-         <Link to={"/myfollowing"}>
-           <span className="material-symbols-outlined">explore</span>{" "}
-         </Link>
-         <Link to="/createpost">
-           {" "}
-           <span className="material-symbols-outlined">add_box</span>{" "}
-         </Link>
+            <Link to={`/myprofile`}>
+              <span className="material-symbols-outlined">person</span>{" "}
+            </Link>
 
-         <span
-           className="material-symbols-outlined log"
-           onClick={() => {
-             setshowLogout(true);
-           }}
-         >
-           logout
-         </span>
-       </>
-     );
-   } else {
-     return (
-       <>
-         <Link to="/signup">
-           {" "}
-           <li>SignUp</li>
-         </Link>
-         <Link to="/login">
-           {" "}
-           <li>Login</li>
-         </Link>
-       </>
-     );
-   }
- };
+            <Link to={"/myfollowing"}>
+              <span className="material-symbols-outlined">explore</span>{" "}
+            </Link>
+            <Link to="/createpost">
+              {" "}
+              <span className="material-symbols-outlined">add_box</span>{" "}
+            </Link>
+
+            <span
+              className="material-symbols-outlined log"
+              onClick={() => {
+                setshowLogout(true);
+              }}
+            >
+              logout
+            </span>
+          </div>
+      );
+    } 
+  };
   return (
     <>
       <div className="nav-bar">
-       
-        <img className='nav-logo' src={Insta} onClick={() => {
-          navigate('/')
+        <img
+          className="nav-logo"
+          src={Insta}
+          onClick={() => {
+            navigate("/");
+          }}
+          alt=""
+        />
 
-      }} alt="" /> 
+<ul className="nav-menu">{loginStatus()}</ul>
 
-        <ul className="nav-menu">
-{           loginStatus()
-}
-        </ul>
-              </div>
+  <ul >{loginMobile()}</ul>
 
-        <ul className='nav-mobile'>
- {
-            loginMobile()
-    }  
+      </div>
 
-        </ul>
-     
     </>
   );
 }
